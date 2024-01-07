b8d1ec23146b5cf786b7143c40893ba8bc80fed0
ismailjosim <ismailjosim99@gmail.com>
2024-01-07 20:57:42 +0600

    array types in typescript

--- module-01/dist/index.js
+++ module-01/dist/index.js
@@ -3,3 +3,13 @@
 // 1. string
 const personName = 'Hello Bangladesh';
 console.log(personName);
+// number type
+const rollNumber = 123;
+// boolean type
+const isAdmin = false;
+// implicit type
+let personName02 = 'John';
+// explicit type
+let exPersonName = 'john';
+// any type (Not recommended)
+let anyName; // any type
---
+++ module-01/src/array.ts
@@ -0,0 +1,12 @@
+// reference type (array)
+
+// string type array
+const names: string[] = ['ismail', 'josim', 'array']
+
+// number types array
+const rollNumbers: number[] = [44, 3, 2, 323, 23, 2323]
+
+// multiple types of array
+const multipleValues = ['abul', 'kabul', 22, 1321, 5435, undefined]
+
+// multipleValues.push(null) show warning. we can push only the data types that inside the array.
