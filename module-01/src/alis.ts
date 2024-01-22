6738f11bf0965af31906a92d35f85054c9e7fb17
ismailjosim <ismailjosim99@gmail.com>
2024-01-23 00:45:17 +0600

    type alias is TS

---
+++ module-01/src/alis.ts
@@ -0,0 +1,59 @@
+const crash1: {
+	name: string
+	age?: number // optional type
+	profession: string
+	address: string
+} = {
+	name: 'pakhi',
+	age: 22,
+	profession: 'web developer',
+	address: 'bangladesh',
+}
+const crash2: {
+	name: string
+	age?: number // optional type
+	profession: string
+	address: string
+} = {
+	name: 'pori',
+	age: 22,
+	profession: 'web developer',
+	address: 'bangladesh',
+}
+// to avoid this type of type declaration we use type alis. for that we use  a type then specify all the properties name there.
+
+type EmployInfo = {
+	name: string
+	age?: number // optional type
+	designation: string
+	address: string
+}
+
+const employOne: EmployInfo = {
+	name: 'ismail',
+	age: 25,
+	designation: 'Web Instructor',
+	address: 'Char fasson, Bhola',
+}
+const employTwo: EmployInfo = {
+	name: 'josim',
+	age: 26,
+	designation: 'Web Instructor L2',
+	address: 'Dhaka Bangladesh',
+}
+
+// type alias with function
+type OperationType = (x: number, y: number) => number
+
+const calcMath = (
+	numberOne: number,
+	numberTwo: number,
+	operation: OperationType,
+) => {
+	return operation(numberOne, numberTwo)
+}
+
+// calling the function with operation
+
+const addFn = calcMath(10, 20, (x, y) => x + y)
+const summationFn = calcMath(30, 10, (x, y) => x - y)
---
+++ readme.md
@@ -0,0 +1,5 @@
+# to run any file with ts-node-dev
+
+```Run
+npx ts-node-dev --respawn src/filename.ts
+```
