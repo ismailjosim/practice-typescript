a4be76ff52005c7e0718df3f17ad4b30b04593b9
ismailjosim <ismailjosim99@gmail.com>
2024-02-08 10:11:39 +0600

    type interface

--- project/src/module-03/interface.ts
+++ project/src/module-03/interface.ts
@@ -24,3 +24,30 @@ const userWithTypeAlias: User = {
 	name: 'ismail',
 	age: 20,
 }
+
+// it also works with object, function and array
+
+// function type with type alias
+// type addNumbersType = (num1: number, num2: number) => number
+
+// const addNumbers: addNumbersType = (num1, num2) => num1 + num2
+
+// function type with type interface
+interface IAddNumbersType {
+	(num1: number, num2: number): number
+}
+
+const addNumbers: IAddNumbersType = (num1, num2) => num1 + num2
+
+//*** comment: we will use type alias in function
+
+// Array (type alias)
+//
+// const rollNumbers2: rollNumbersType = [1, 4, 5]
+
+// Array (type interface)
+type rollNumbersType = number[]
+interface IRollNumbers {
+	[index: number]: number
+}
+const rollNumbers2: IRollNumbers = [1, 4, 5]
