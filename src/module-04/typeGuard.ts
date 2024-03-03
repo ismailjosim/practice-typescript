01ccfa461ddb084bee6aab89b39954908e7cde39
ismailjosim <ismailjosim99@gmail.com>
2024-03-03 11:09:14 +0600

    typeGuard In Typescript

--- src/module-04/typeGuard.ts
+++ src/module-04/typeGuard.ts
@@ -15,10 +15,82 @@ addTwoNum(3, 4)
 
 type NormalUserType = {
 	name: string
-	role: 'member'
 }
 
 type AdminUserType = {
 	name: string
 	role: 'admin'
 }
+
+function getUser(user: NormalUserType | AdminUserType): string {
+	if ('role' in user) {
+		return `This is ${user.name} and my role is ${user.role}`
+	} else {
+		return `This ${user.name} and i'm a Normal User`
+	}
+}
+
+const normalUserOne: NormalUserType = { name: 'MR. X' }
+const adminUserOne: AdminUserType = { name: 'ismail', role: 'admin' }
+
+// console.log(getUser(normalUserOne))
+// console.log(getUser(adminUserOne))
+
+// instance of guard (it guard class and object)
+
+class Animal {
+	name: string
+	species: string
+	sound: string
+	constructor(name: string, species: string, sound: string) {
+		this.name = name
+		this.species = species
+		this.sound = sound
+	}
+
+	makeSound() {
+		return `${this.name} make sound ${this.sound}`
+	}
+}
+
+class Dog extends Animal {
+	constructor(name: string, species: string, sound: string) {
+		super(name, species, sound)
+	}
+
+	makeGuard() {
+		console.log(`The ${this.name} Dog Guard our Garden and ${this.makeSound()}`)
+	}
+}
+class Cat extends Animal {
+	constructor(name: string, species: string, sound: string) {
+		super(name, species, sound)
+	}
+
+	sleep() {
+		console.log(`The ${this.name} Cat is always sleep and ${this.makeSound()}`)
+	}
+}
+
+// check type guard in a class and object
+function getAnimal(animal: Animal) {
+	if (animal instanceof Dog) {
+		animal.makeSound()
+		animal.makeGuard()
+	} else if (isCat(animal)) {
+		animal.makeSound()
+		animal.sleep()
+	} else {
+		animal.makeSound()
+	}
+}
+
+// check type using function
+function isCat(animal: Animal): animal is Cat {
+	return animal instanceof Cat
+}
+
+const animalOne = new Dog('German Shepard', 'dog', 'gew')
+const animalTwo = new Cat('Persian', 'Cat', 'Mewa')
+getAnimal(animalOne)
+getAnimal(animalTwo)
